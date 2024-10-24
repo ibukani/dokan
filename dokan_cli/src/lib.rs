10148de3037477099b3315fd9d9d490ec54e4e44
ibukani <ibukani2056@gmail.com>
2024-10-25 02:03:20 +0900

    cliの表示部分を別の場所へ

--- dokan_cli/src/lib.rs
+++ dokan_cli/src/lib.rs
@@ -1,14 +1,22 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::process::ExitCode;
+use clap::Parser;
+use crate::command::add::add;
+use crate::command::Commands;
+
+pub mod command;
+
+#[derive(Parser)]
+#[command(version, about)]
+struct Args {
+    #[command(subcommand)]
+    command: Commands
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+pub fn run() -> ExitCode {
+    match Args::parse().command {
+        Commands::Add {} => add(),
+        _ => {}
     }
+
+    ExitCode::SUCCESS
 }
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,5 @@
-mod command;
+use dokan_cli::run;
 
-use clap::{Parser};
-use crate::command::add::add_command;
-use crate::command::Commands;
-
-#[derive(Parser)]
-#[command(version, about)]
-struct Args {
-    #[command(subcommand)]
-    command: Commands
-}
-
-
-fn main() -> Result<(), ()> {
-    match Args::parse().command {
-        Commands::Add { }  => add_command(),
-        _ => {}
-    };
-
-    Ok(())
-}
-
-pub fn add_project() {
-
+fn main()  {
+    run();
 }
